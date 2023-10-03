dd32ba9484649c05d9b33f994b536ded72af5724
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2023-10-03 22:56:59 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+
+// import Feedback from './components/Feedback/';
+// import Statistics from './components/Statistics/';
+// import Section from './components/SectionTitle/';
+
+export class App extends Component {
+  
+  state = {
+    contacts: [],
+    name: '',
+    tel: ''
+  }
+
+  handleNameChange = event => {
+    console.log(event.currentTarget.value);
+    this.setState( {name: event.currentTarget.value} );
+  }
+
+  handleTelChange = event => {
+    console.log(event.currentTarget.value);
+    this.setState( {tel: event.currentTarget.value} );
+  }
+//   onLeaveFeedback = (option) => {
+//         this.setState(prevState => (
+//          {[option]: prevState[option] + 1}
+//         ))
+// }
+
+  render() {
+   
+    // const { good, neutral, bad } = this.state;
+  
+    return (
+      <div>
+        <form> <br/>
+          <label htmlFor="name">
+          Name<input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} required />
+          </label><br/><br/>
+          <label htmlFor="name">
+            Phone<input type="tel" name="tel" value={this.state.tel} onChange={this.handleTelChange} required />
+          </label>
+        </form>
+       
+      </div>
+    )
+  }
+}
